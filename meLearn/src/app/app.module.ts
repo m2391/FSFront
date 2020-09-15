9e3c3d1d7aabc4b95e059dc001ccdef52e4e9b7f
Siina Kaakinen <m2936@student.jamk.fi>
2020-09-15 12:45:26 +0300

    Kaikissa kansioissa komponentti

--- meLearn/src/app/app.module.ts
+++ meLearn/src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login/login.component';
 import { MainComponent } from './main/main/main.component';
+import { HistoryComponent } from './history/history/history.component';
+import { RegisterComponent } from './register/register/register.component';
+import { SettingsComponent } from './settings/settings/settings.component';
+import { StatusComponent } from './status/status/status.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MainComponent
+    MainComponent,
+    HistoryComponent,
+    RegisterComponent,
+    SettingsComponent,
+    StatusComponent
   ],
   imports: [
     BrowserModule,
